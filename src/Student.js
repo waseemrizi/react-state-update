340a6bc9bd3a523d4143063640725591e43a0d80
waseemrizi <waseemrizi@outlook.com>
2020-06-09 19:02:00 +0500

    adding repo h

--- src/Student.js
+++ src/Student.js
@@ -1,38 +1,16 @@
 import React, { Component } from 'react';
 
 
-class Student extends Component{
-        constructor(){
-        super();
-
-        this.state = {
-        name: "Waseem",
-        roll: "2205",
-        subject: "IT"
-    }; 
-
-    
-    }
-    
-
-
-   handlerClick = () => {
-        this.setState({name: "Malik", roll: "105" })
-    };
-    render(){
-
-        return(
-            <div>
-               
-
-                <h1 className="nrs">Hello {this.state.name} Your Roll No is  {this.state.roll} <br></br>Your suject is <strong>{this.state.subject}</strong></h1>
-          
-            <button className="btn1" onClick={this.handlerClick}>Click to Change Name</button>
+function Student(){
+    return(
+        <div>
+            <h1>Hello this is class three</h1>
         </div>
-        );
-    }
-
+    )
 }
+
+
+
+
+
 export default Student;
-
-
--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Student from './Student';
-import './Student.css';
 
 
-
-ReactDOM.render(<Student  />,
-     document.getElementById('root'));
+ReactDOM.render(<Student  />, document.getElementById('root'));
